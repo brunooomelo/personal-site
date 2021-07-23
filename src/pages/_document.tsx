37a7c0e2385dcb9b0736cc93824070a44d695c1b
Bruno Melo <utibmelo@gmail.com>
2021-07-23 01:18:57 -0300

    chore(font): fixing custom font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,16 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Merriweather&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
