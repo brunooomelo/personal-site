1c8164f21e85f2cd9bac93f1cc38ca542f1c95db
Bruno Melo <utibmelo@gmail.com>
2021-07-23 01:05:36 -0300

    feat(pages): change something from _document to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,35 @@
 import '../styles/style.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>brunooomelo - JS DEVELOPER</title>
+        <link rel="shortcut icon" href="/image/icon-512.png" />
+        <link rel="apple-touch-icon" href="/image/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#121212" />
+        <meta name="description" content="The best Game Store in the world!" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Merriweather&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
+      <Script
+        src="https://use.fontawesome.com/releases/v5.8.2/js/all.js"
+        integrity="sha384-DJ25uNYET2XCl5ZF++U8eNxPWqcKohUUBUpKGlNLMchM7q4Wjg2CUpjHLaL8yYPH"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
     </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,24 +3,8 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
-          <script
-            defer
-            src="https://use.fontawesome.com/releases/v5.8.2/js/all.js"
-            integrity="sha384-DJ25uNYET2XCl5ZF++U8eNxPWqcKohUUBUpKGlNLMchM7q4Wjg2CUpjHLaL8yYPH"
-            crossOrigin="anonymous"
-          ></script>
-          <link
-            href="https://fonts.googleapis.com/css?family=Merriweather&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Head>
+      <Html lang="en-US">
+        <Head />
         <body>
           <Main />
           <NextScript />
