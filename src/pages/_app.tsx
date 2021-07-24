05c9d5211ae0b8dd72589831aea955f19a8d9e76
brunooomelo <utibmelo@gmail.com>
2021-07-24 09:53:05 -0300

    feat(inspx): add package and set setting in inspx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/style.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from 'next/script'
+import Inspect from 'inspx'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Inspect disabled={isProd}>
       <Head>
         <title>brunooomelo - JS DEVELOPER</title>
         <link rel="shortcut icon" href="/image/icon-512.png" />
@@ -15,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="The best Game Store in the world!" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </Inspect>
   )
 }
 
