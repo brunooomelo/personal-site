51dd8948f218aaed3573ec564940f8db847204e0
Bruno Melo <utibmelo@gmail.com>
2021-07-29 00:31:09 -0300

    feat(framer-motion): add animations

---
+++ src/common/AnchorIcon.tsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion'
+
+interface AnchorProps {
+  label: string
+  url: string
+  children: React.ReactChild
+}
+
+export function AnchorIcon({ label, url, children }: AnchorProps) {
+  return (
+    <motion.a
+      href={url}
+      target="_blank"
+      className="box-social"
+      rel="noreferrer"
+      aria-label={label}
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 0.5, y: 0 }}
+      exit={{ opacity: 0, y: 0 }}
+      whileHover={{ opacity: 1, scale: 1.3 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      {children}
+    </motion.a>
+  )
+}
--- src/pages/en/index.tsx
+++ src/pages/en/index.tsx
@@ -1,51 +1,61 @@
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 import { SiGithub, SiLinkedin, SiTwitter } from 'react-icons/si'
+import { AnchorIcon } from '../../common/AnchorIcon'
 
 const IconProps = {
   size: 48,
   color: '#fff',
 }
-
 const HomePage = () => (
   <div className="container">
     <header className="header">
-      <Link href="/" locale="pt-br">
-        <a className="btn-translate">pt</a>
+      <Link href="/" locale="en" passHref>
+        <motion.a
+          initial={{ opacity: 0, y: 100, scale: 1.5 }}
+          animate={{ opacity: 0.8, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: 0 }}
+          transition={{ duration: 1 }}
+          whileHover={{ opacity: 1, scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="btn-translate"
+        >
+          en
+        </motion.a>
       </Link>
     </header>
     <div className="content">
-      <div className="title">bruno melo</div>
-      <div className="description">
+      <motion.div
+        className="title"
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 0.5, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        bruno melo
+      </motion.div>
+      <motion.div
+        className="description"
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 0.5, y: 0 }}
+        transition={{ duration: 1 }}
+      >
         Javascript Developer and other awesome things.
-      </div>
+      </motion.div>
       <div className="container-social">
-        <a
-          href="https://github.com/brunooomelo"
-          target="_blank"
-          className="box-social"
-          rel="noreferrer"
-          aria-label="Github"
-        >
+        <AnchorIcon url="https://github.com/brunooomelo" label="Github">
           <SiGithub {...IconProps} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/brunooomelo/"
-          target="_blank"
-          className="box-social"
-          rel="noreferrer"
-          aria-label="LinkedIn"
+        </AnchorIcon>
+
+        <AnchorIcon
+          url="https://www.linkedin.com/in/brunooomelo"
+          label="LinkedIn"
         >
           <SiLinkedin {...IconProps} />
-        </a>
-        <a
-          href="https://twitter.com/brunooomelo"
-          target="_blank"
-          className="box-social"
-          rel="noreferrer"
-          aria-label="Twitter"
-        >
+        </AnchorIcon>
+
+        <AnchorIcon url="https://twitter.com/brunooomelo" label="Twitter">
           <SiTwitter {...IconProps} />
-        </a>
+        </AnchorIcon>
       </div>
     </div>
   </div>
