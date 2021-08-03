b22683d6920a5edf0f254a23073727313b9180c7
Bruno Melo <bruno94@outlook.com>
2021-08-03 20:57:55 -0300

    feat(bundle-analyze): add package and configuration

--- next.config.js
+++ next.config.js
@@ -1,34 +1,39 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  i18n: {
-    locales: ['en-us', 'pt-br'],
-    defaultLocale: 'pt-br',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/en-us',
-        destination: '/en',
-        locale: false,
-      },
-      {
-        source: '/pt-br',
-        destination: '/',
-        locale: false,
-      },
-    ]
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd,
-  },
-  images: {
-    domains: ['localhost', 'brunooomelo.com'],
-  },
-  webpack5: true,
-  env: {
-    PREVIEW: process.env.PREVIEW,
-  },
-})
+module.exports = withBundleAnalyzer(
+  withPWA({
+    i18n: {
+      locales: ['en-us', 'pt-br'],
+      defaultLocale: 'pt-br',
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/en-us',
+          destination: '/en',
+          locale: false,
+        },
+        {
+          source: '/pt-br',
+          destination: '/',
+          locale: false,
+        },
+      ]
+    },
+    pwa: {
+      dest: 'public',
+      disable: !isProd,
+    },
+    images: {
+      domains: ['localhost', 'brunooomelo.com'],
+    },
+    webpack5: true,
+    env: {
+      PREVIEW: process.env.PREVIEW,
+    },
+  })
+)
